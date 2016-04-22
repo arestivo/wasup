d8d95239f95028bcdb6318e0b36261d7c7cf0f2f
Andre Restivo <andre.restivo@gmail.com>
2016-04-22 16:29:31 +0100

    Countodnw timer

--- index.php
+++ index.php
@@ -9,8 +9,37 @@ include('html/header.html');
 
 if ($wasup->testUploadPerms()) {
 	$wasup->assignment->listUploads();
+  echo '<p>Delivery by: '.$wasup->assignment->date.'</p>';
+  echo '<p>Time remaining: <span id="remaining"></span></p>';
 	echo '<form><a class="btn" href="upload.php">Upload Assignment</a></form>';
 }
+?>
+<script>
+  updateRemaining();
+  function updateRemaining() {
+    var d1 = new Date('<?=$wasup->assignment->date?>');
+    var d2 = new Date();
+    var delta = Math.floor(d1.getTime() - d2.getTime()) / 1000;
+    if (delta < 0) {
+      document.getElementById('remaining').innerHTML = "Ended";
+      return;
+    }
+    var days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+    var hours = Math.floor(delta / 3600) % 24;
+    delta -= hours * 3600;
+    var minutes = Math.floor(delta / 60) % 60;
+    delta -= minutes * 60;
+    var seconds = Math.floor(delta % 60);
 
+    document.getElementById('remaining').innerHTML = days + " days " + 
+        (hours < 10 ? "0" + hours : hours) + ":" + 
+        (minutes < 10 ? "0" + minutes : minutes) + ":" + 
+        (seconds < 10 ? "0" + seconds : seconds);
+    setInterval(updateRemaining, 1000);
+  }
+</script>
+<?php
 include('html/footer.html');
 ?>
+
