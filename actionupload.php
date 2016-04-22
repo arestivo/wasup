af4dc876f0a7bb19722c7143d42adb5850c6b634
Andre Restivo <andre.restivo@gmail.com>
2016-04-22 09:44:46 +0100

    Verify is user is logged in before uploading

--- actionupload.php
+++ actionupload.php
@@ -5,6 +5,8 @@ require_once('classes/Wasup.php');
 $wasup = new Wasup();
 $wasup->readConfiguration();
 
+if (!$wasup->authenticator->isLoggedIn()) die(header('Location: login.php'));
+
 $wasup->assignment->handleUpload();
 
 header("Location: index.php");
